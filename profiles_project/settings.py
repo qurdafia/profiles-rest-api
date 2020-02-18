17c918faaab95b3024da7488fd7f39a3fc9f4c64
Mark <contact@unrestph.com>
2020-02-18 14:55:28 +0800

    Creating a user model.

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework.authtoken',
-    
+
     'profiles_api',
 ]
 
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
